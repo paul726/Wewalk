e8626ded8c0c9942c2fa4d577965ec9b3f020b01
Paul Jiang <paulji@ext.inditex.com>
2024-08-07 22:28:44 +0800

    login entrance

--- app/src/main/java/com/nus/wewalk/ui/me/MineFragment.java
+++ app/src/main/java/com/nus/wewalk/ui/me/MineFragment.java
@@ -1,5 +1,6 @@
 package com.nus.wewalk.ui.me;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,10 +8,12 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.nus.wewalk.databinding.FragmentMineBinding;
+import com.nus.wewalk.ui.login.LoginActivity;
 
 public class MineFragment extends Fragment {
 
@@ -23,10 +26,19 @@ public class MineFragment extends Fragment {
 
         binding = FragmentMineBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        final TextView textView = binding.textNotifications;
-        mineViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.textNotifications.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
